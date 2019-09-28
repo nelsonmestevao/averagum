82a00bc8d236436066b6a5ec4728bf7e3d09ecbd
Nelson Estevão <nelsonmestevao@gmail.com>
2019-09-28 14:47:21 +0100

    Start using lambda function

--- averagum.py
+++ averagum.py
@@ -10,8 +10,7 @@ weights = pd.read_csv(DATA_PATH + "weights.csv")
 
 courses = courses.dropna()
 
-def multiplier(type):
-    return weights[weights.Área_Científica == type].Ponderação.values[0]
+multiplier = lambda type: weights[weights.Área_Científica == type].Ponderação.values[0]
 
 total = 0
 soma = 0
@@ -20,5 +19,4 @@ for index, row in courses.iterrows():
     soma += row.Classificação * multiplier(row.Área_Científica)
     total += multiplier(row.Área_Científica)
 
-print("A tua média ponderada é:", soma/total)
-
+print("A tua média ponderada é:", soma / total)
